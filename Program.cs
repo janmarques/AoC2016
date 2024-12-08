1ffbc1e59b2e1ab604b98c67ba80708b261bc767
Jan Marques <jan@janmarques.net>
2024-12-08 15:23:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿var fullInput =
+@"R5, L2, L1, R1, R3, R3, L3, R3, R4, L2, R4, L4, R4, R3, L2, L1, L1, R2, R4, R4, L4, R3, L2, R1, L4, R1, R3, L5, L4, L5, R3, L3, L1, L1, R4, R2, R2, L1, L4, R191, R5, L2, R46, R3, L1, R74, L2, R2, R187, R3, R4, R1, L4, L4, L2, R4, L5, R4, R3, L2, L1, R3, R3, R3, R1, R1, L4, R4, R1, R5, R2, R1, R3, L4, L2, L2, R1, L3, R1, R3, L5, L3, R5, R3, R4, L1, R3, R2, R1, R2, L4, L1, L1, R3, L3, R4, L2, L4, L5, L5, L4, R2, R5, L4, R4, L2, R3, L4, L3, L5, R5, L4, L2, R3, R5, R5, L1, L4, R3, L1, R2, L5, L1, R4, L1, R5, R1, L4, L4, L4, R4, R3, L5, R1, L3, R4, R3, L2, L1, R1, R2, R2, R2, L1, L1, L2, L5, L3, L1";
+
+var smallInput =
+@"R2, R2, R2";
+
+var smallest = "";
+
+var input = smallInput;
+input = fullInput;
+//input = smallest;
+var timer = System.Diagnostics.Stopwatch.StartNew();
+
+var result = 0;
+
+var direction = 0;
+var x = 0;
+var y = 0;
+foreach (var item in input.Split(", "))
+{
+    var letter = item.First();
+    var number = int.Parse(new string(item.Skip(1).ToArray()));
+    direction = (360 + direction + (letter == 'R' ? 90 : -90)) % 360;
+    switch (direction)
+    {
+        case 0:
+            y += number;
+            break;
+        case 90:
+            x += number;
+            break;
+        case 180:
+            y -= number;
+            break;
+        case 270:
+            x -= number;
+            break;
+        default: throw new Exception();
+    }
+}
+
+result = Math.Abs(x) + Math.Abs(y);
+timer.Stop();
+Console.WriteLine(result);
+Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+Console.ReadLine();
+
+void PrintGrid<T>(T[][] grid)
+{
+    for (int i = 0; i < grid.Length; i++)
+    {
+        for (int j = 0; j < grid[i].Length; j++)
+        {
+            Console.Write(grid[i][j]);
+        }
+        Console.WriteLine();
+    }
+}
