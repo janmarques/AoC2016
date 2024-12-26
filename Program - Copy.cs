e7b67f9c93754fc3abdaeae7ec85645983b9144e
Jan Marques <jan@janmarques.net>
2024-12-26 17:40:41 +0100

    d

--- Program - Copy.cs
+++ Program - Copy.cs
@@ -4,7 +4,8 @@
 //var smallInput =
 //@"";
 
-//var smallest = "";
+//var smallest = 
+//@"";
 
 //var input = smallInput;
 ////input = fullInput;
@@ -22,15 +23,3 @@
 //Console.WriteLine(result);
 //Console.WriteLine(timer.ElapsedMilliseconds + "ms");
 //Console.ReadLine();
-
-//void PrintGrid<T>(T[][] grid)
-//{
-//    for (int i = 0; i < grid.Length; i++)
-//    {
-//        for (int j = 0; j < grid[i].Length; j++)
-//        {
-//            Console.Write(grid[i][j]);
-//        }
-//        Console.WriteLine();
-//    }
-//}
