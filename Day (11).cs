7d860a697d1e185d099dc2031cccf19635d53466
Jan Marques <jan@janmarques.net>
2024-12-27 19:35:38 +0100

    dd

--- Day (11).cs
+++ Day (11).cs
@@ -1,109 +1,109 @@
-﻿using AoC2023;
-using AoC2024;
+﻿//using AoC2023;
+//using AoC2024;
 
-var fullInput =
-@"polonium-G thulium-G thulium-C promethium-G ruthenium-G ruthenium-C cobalt-G cobalt-C elerium-G elerium-C dilithium-G dilithium-C
-polonium-C promethium-C
+//var fullInput =
+//@"polonium-G thulium-G thulium-C promethium-G ruthenium-G ruthenium-C cobalt-G cobalt-C elerium-G elerium-C dilithium-G dilithium-C
+//polonium-C promethium-C
 
-";
+//";
 
-var smallInput =
-@"hydrogen-C lithium-C
-hydrogen-G
-lithium-G
-";
+//var smallInput =
+//@"hydrogen-C lithium-C
+//hydrogen-G
+//lithium-G
+//";
 
-var smallest =
-@"";
+//var smallest =
+//@"";
 
-var input = smallInput;
-input = fullInput;
-//input = smallest;
-var timer = System.Diagnostics.Stopwatch.StartNew();
+//var input = smallInput;
+//input = fullInput;
+////input = smallest;
+//var timer = System.Diagnostics.Stopwatch.StartNew();
 
-var result = 0;
+//var result = 0;
 
-var floors = input.Split(Environment.NewLine).Select(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList()).ToList();
+//var floors = input.Split(Environment.NewLine).Select(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList()).ToList();
 
-var queue = new PrioritySetHashed<State, long>(x => x.ToString());
-queue.Enqueue(new State { Elevator = 0, Positions = floors }, 0);
+//var queue = new PrioritySetHashed<State, long>(x => x.ToString());
+//queue.Enqueue(new State { Elevator = 0, Positions = floors }, 0);
 
-//var visited = new HashSet<string>();
+////var visited = new HashSet<string>();
 
-while (queue.Count > 0)
-{
-    var state = queue.Dequeue();
-    //visited.Add(item.ToString());
+//while (queue.Count > 0)
+//{
+//    var state = queue.Dequeue();
+//    //visited.Add(item.ToString());
 
-    if (!state.Positions[0].Any() && !state.Positions[1].Any() && !state.Positions[2].Any())
-    {
-        Console.WriteLine(state.Steps);
-    }
+//    if (!state.Positions[0].Any() && !state.Positions[1].Any() && !state.Positions[2].Any())
+//    {
+//        Console.WriteLine(state.Steps);
+//    }
 
-    void TryQueue(params string[] items)
-    {
-        if (!CanSurvive(state.Positions[state.Elevator].Except(items)))
-        {
-            return;
-        }
-        if (state.Elevator > 0 && CanSurvive(state.Positions[state.Elevator - 1].Union(items)))
-        {
-            var newState = state.Clone();
-            newState.Positions[newState.Elevator].RemoveAll(x => items.Contains(x));
-            newState.Positions[newState.Elevator - 1].AddRange(items);
-            newState.Elevator--;
-            newState.Steps++;
-            queue.Enqueue(newState, state.Steps);
-        }
+//    void TryQueue(params string[] items)
+//    {
+//        if (!CanSurvive(state.Positions[state.Elevator].Except(items)))
+//        {
+//            return;
+//        }
+//        if (state.Elevator > 0 && CanSurvive(state.Positions[state.Elevator - 1].Union(items)))
+//        {
+//            var newState = state.Clone();
+//            newState.Positions[newState.Elevator].RemoveAll(x => items.Contains(x));
+//            newState.Positions[newState.Elevator - 1].AddRange(items);
+//            newState.Elevator--;
+//            newState.Steps++;
+//            queue.Enqueue(newState, state.Steps);
+//        }
 
-        if (state.Elevator < 3 && CanSurvive(state.Positions[state.Elevator + 1].Union(items)))
-        {
-            var newState = state.Clone();
-            newState.Positions[newState.Elevator].RemoveAll(x => items.Contains(x));
-            newState.Positions[newState.Elevator + 1].AddRange(items);
-            newState.Elevator++;
-            newState.Steps++;
-            queue.Enqueue(newState, state.Steps);
-        }
-    }
+//        if (state.Elevator < 3 && CanSurvive(state.Positions[state.Elevator + 1].Union(items)))
+//        {
+//            var newState = state.Clone();
+//            newState.Positions[newState.Elevator].RemoveAll(x => items.Contains(x));
+//            newState.Positions[newState.Elevator + 1].AddRange(items);
+//            newState.Elevator++;
+//            newState.Steps++;
+//            queue.Enqueue(newState, state.Steps);
+//        }
+//    }
 
-    bool CanSurvive(IEnumerable<string> items1, params string[] items2)
-    {
-        var allItems = items1.Concat(items2);
-        var chips = allItems.Where(IsChip);
-        var generators = allItems.Where(x => !IsChip(x));
-        if (!generators.Any() || !chips.Any()) { return true; }
-        var unpairedChips = chips.Where(x => !generators.Contains(GenerateGeneratorName(Name(x))));
-        return !unpairedChips.Any();
-    }
+//    bool CanSurvive(IEnumerable<string> items1, params string[] items2)
+//    {
+//        var allItems = items1.Concat(items2);
+//        var chips = allItems.Where(IsChip);
+//        var generators = allItems.Where(x => !IsChip(x));
+//        if (!generators.Any() || !chips.Any()) { return true; }
+//        var unpairedChips = chips.Where(x => !generators.Contains(GenerateGeneratorName(Name(x))));
+//        return !unpairedChips.Any();
+//    }
 
-    bool IsChip(string s) => s.EndsWith("-C");
-    string Name(string s) => s.Split("-").First();
-    string GenerateGeneratorName(string s) => s + "-G";
+//    bool IsChip(string s) => s.EndsWith("-C");
+//    string Name(string s) => s.Split("-").First();
+//    string GenerateGeneratorName(string s) => s + "-G";
 
-    foreach (var item in state.Positions[state.Elevator])
-    {
-        TryQueue(item);
-        foreach (var otherItem in state.Positions[state.Elevator].Where(x => x != item))
-        {
-            TryQueue(item, otherItem);
-        }
-    }
-}
+//    foreach (var item in state.Positions[state.Elevator])
+//    {
+//        TryQueue(item);
+//        foreach (var otherItem in state.Positions[state.Elevator].Where(x => x != item))
+//        {
+//            TryQueue(item, otherItem);
+//        }
+//    }
+//}
 
-timer.Stop();
-Console.WriteLine(result);
-Console.WriteLine(timer.ElapsedMilliseconds + "ms");
-Console.ReadLine();
+//timer.Stop();
+//Console.WriteLine(result);
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+//Console.ReadLine();
 
 
-class State
-{
-    public int Elevator { get; set; }
-    public List<List<string>> Positions { get; set; }
-    public long Steps { get; set; }
+//class State
+//{
+//    public int Elevator { get; set; }
+//    public List<List<string>> Positions { get; set; }
+//    public long Steps { get; set; }
 
-    public State Clone() => new State { Elevator = Elevator, Steps = Steps, Positions = Positions.Select(x => x.ToList()).ToList() };
+//    public State Clone() => new State { Elevator = Elevator, Steps = Steps, Positions = Positions.Select(x => x.ToList()).ToList() };
 
-    public override string ToString() => $"{Elevator}+{string.Join("|", Positions.Select((x, i) => $"{i}{(string.Join(",", x.OrderBy(y => y)))}"))}";
-}
+//    public override string ToString() => $"{Elevator}+{string.Join("|", Positions.Select((x, i) => $"{i}{(string.Join(",", x.OrderBy(y => y)))}"))}";
+//}
--- Day (12).cs
+++ Day (12).cs
@@ -1,21 +1,106 @@
-﻿//var fullInput =
-//@"";
+﻿var fullInput =
+@"cpy 1 a
+cpy 1 b
+cpy 26 d
+jnz c 2
+jnz 1 5
+cpy 7 c
+inc d
+dec c
+jnz c -2
+cpy a c
+inc a
+dec b
+jnz b -2
+cpy c b
+dec d
+jnz d -6
+cpy 17 c
+cpy 18 d
+inc a
+dec d
+jnz d -2
+dec c
+jnz c -5";
 
-//var smallInput =
-//@"";
+var smallInput =
+@"cpy 41 a
+inc a
+inc a
+dec a
+jnz a 2
+dec a";
 
-//var smallest = "";
+var smallest =
+@"";
 
-//var input = smallInput;
-////var input = fullInput;
-////var input = smallest;
+var input = smallInput;
+//input = fullInput;
+//input = smallest;
+var timer = System.Diagnostics.Stopwatch.StartNew();
 
-//var result = "";
+var result = 0;
 
-//foreach (var line in input.Split(Environment.NewLine))
-//{
+var asm = new Assembunny();
+asm.Execute(input.Split(Environment.NewLine).ToArray());
 
-//}
+result = asm.Registers['a'];
 
-//Console.WriteLine(result);
-//Console.ReadLine();
+timer.Stop();
+Console.WriteLine(result);
+Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+Console.ReadLine();
+
+class Assembunny
+{
+    public Dictionary<char, int> Registers { get; set; } = new Dictionary<char, int>();
+    public void Execute(string[] lines)
+    {
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            var split = line.Split(" ").ToArray();
+            var op = split[0];
+            if (op == "cpy")
+            {
+                var value = 0;
+                if (split[1].Length > 1 || !char.IsLetter(split[1][0]))
+                {
+                    value = int.Parse(split[1]);
+                }
+                else
+                {
+                    value = SafeGet(split[1].Single());
+                }
+                SafeSet(split[2].Single(), value);
+            }
+            if (op == "inc")
+            {
+                var key = split[1].Single();
+                SafeSet(key, SafeGet(key) + 1);
+            }
+            if (op == "dec")
+            {
+                var key = split[1].Single();
+                SafeSet(key, SafeGet(key) - 1);
+            }
+            if (op == "jnz")
+            {
+                if (SafeGet(split[1].Single()) != 0)
+                {
+                    i += int.Parse(split[2]) - 1;
+                }
+            }
+        }
+    }
+    void SafeSet(char c, int value)
+    {
+        if (!Registers.ContainsKey(c)) { Registers[c] = 0; }
+    }
+
+    int SafeGet(char c)
+    {
+        if (!Registers.ContainsKey(c)) { Registers[c] = 0; }
+        return Registers[c];
+    }
+}
