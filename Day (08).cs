9107c783941c05b116770de9178a640f4a24ae5b
Jan Marques <jan@janmarques.net>
2024-12-27 15:45:35 +0100

    d9p1

--- Day (08).cs
+++ Day (08).cs
@@ -1,257 +1,257 @@
-﻿using AoC2024;
+﻿//using AoC2024;
 
-var fullInput =
-@"rect 1x1
-rotate row y=0 by 20
-rect 1x1
-rotate row y=0 by 2
-rect 1x1
-rotate row y=0 by 3
-rect 2x1
-rotate row y=0 by 2
-rect 1x1
-rotate row y=0 by 3
-rect 2x1
-rotate row y=0 by 2
-rect 1x1
-rotate row y=0 by 4
-rect 2x1
-rotate row y=0 by 2
-rect 1x1
-rotate row y=0 by 2
-rect 1x1
-rotate row y=0 by 2
-rect 1x1
-rotate row y=0 by 3
-rect 2x1
-rotate row y=0 by 2
-rect 1x1
-rotate row y=0 by 5
-rect 1x1
-rotate row y=0 by 2
-rect 1x1
-rotate row y=0 by 6
-rect 5x1
-rotate row y=0 by 2
-rect 1x3
-rotate row y=2 by 8
-rotate row y=0 by 8
-rotate column x=0 by 1
-rect 7x1
-rotate row y=2 by 24
-rotate row y=0 by 20
-rotate column x=5 by 1
-rotate column x=4 by 2
-rotate column x=2 by 2
-rotate column x=0 by 1
-rect 7x1
-rotate column x=34 by 2
-rotate column x=22 by 1
-rotate column x=15 by 1
-rotate row y=2 by 18
-rotate row y=0 by 12
-rotate column x=8 by 2
-rotate column x=7 by 1
-rotate column x=5 by 2
-rotate column x=2 by 1
-rotate column x=0 by 1
-rect 9x1
-rotate row y=3 by 28
-rotate row y=1 by 28
-rotate row y=0 by 20
-rotate column x=18 by 1
-rotate column x=15 by 1
-rotate column x=14 by 1
-rotate column x=13 by 1
-rotate column x=12 by 2
-rotate column x=10 by 3
-rotate column x=8 by 1
-rotate column x=7 by 2
-rotate column x=6 by 1
-rotate column x=5 by 1
-rotate column x=3 by 1
-rotate column x=2 by 2
-rotate column x=0 by 1
-rect 19x1
-rotate column x=34 by 2
-rotate column x=24 by 1
-rotate column x=23 by 1
-rotate column x=14 by 1
-rotate column x=9 by 2
-rotate column x=4 by 2
-rotate row y=3 by 5
-rotate row y=2 by 3
-rotate row y=1 by 7
-rotate row y=0 by 5
-rotate column x=0 by 2
-rect 3x2
-rotate column x=16 by 2
-rotate row y=3 by 27
-rotate row y=2 by 5
-rotate row y=0 by 20
-rotate column x=8 by 2
-rotate column x=7 by 1
-rotate column x=5 by 1
-rotate column x=3 by 3
-rotate column x=2 by 1
-rotate column x=1 by 2
-rotate column x=0 by 1
-rect 9x1
-rotate row y=4 by 42
-rotate row y=3 by 40
-rotate row y=1 by 30
-rotate row y=0 by 40
-rotate column x=37 by 2
-rotate column x=36 by 3
-rotate column x=35 by 1
-rotate column x=33 by 1
-rotate column x=32 by 1
-rotate column x=31 by 3
-rotate column x=30 by 1
-rotate column x=28 by 1
-rotate column x=27 by 1
-rotate column x=25 by 1
-rotate column x=23 by 3
-rotate column x=22 by 1
-rotate column x=21 by 1
-rotate column x=20 by 1
-rotate column x=18 by 1
-rotate column x=17 by 1
-rotate column x=16 by 3
-rotate column x=15 by 1
-rotate column x=13 by 1
-rotate column x=12 by 1
-rotate column x=11 by 2
-rotate column x=10 by 1
-rotate column x=8 by 1
-rotate column x=7 by 2
-rotate column x=5 by 1
-rotate column x=3 by 3
-rotate column x=2 by 1
-rotate column x=1 by 1
-rotate column x=0 by 1
-rect 39x1
-rotate column x=44 by 2
-rotate column x=42 by 2
-rotate column x=35 by 5
-rotate column x=34 by 2
-rotate column x=32 by 2
-rotate column x=29 by 2
-rotate column x=25 by 5
-rotate column x=24 by 2
-rotate column x=19 by 2
-rotate column x=15 by 4
-rotate column x=14 by 2
-rotate column x=12 by 3
-rotate column x=9 by 2
-rotate column x=5 by 5
-rotate column x=4 by 2
-rotate row y=5 by 5
-rotate row y=4 by 38
-rotate row y=3 by 10
-rotate row y=2 by 46
-rotate row y=1 by 10
-rotate column x=48 by 4
-rotate column x=47 by 3
-rotate column x=46 by 3
-rotate column x=45 by 1
-rotate column x=43 by 1
-rotate column x=37 by 5
-rotate column x=36 by 5
-rotate column x=35 by 4
-rotate column x=33 by 1
-rotate column x=32 by 5
-rotate column x=31 by 5
-rotate column x=28 by 5
-rotate column x=27 by 5
-rotate column x=26 by 3
-rotate column x=25 by 4
-rotate column x=23 by 1
-rotate column x=17 by 5
-rotate column x=16 by 5
-rotate column x=13 by 1
-rotate column x=12 by 5
-rotate column x=11 by 5
-rotate column x=3 by 1
-rotate column x=0 by 1";
+//var fullInput =
+//@"rect 1x1
+//rotate row y=0 by 20
+//rect 1x1
+//rotate row y=0 by 2
+//rect 1x1
+//rotate row y=0 by 3
+//rect 2x1
+//rotate row y=0 by 2
+//rect 1x1
+//rotate row y=0 by 3
+//rect 2x1
+//rotate row y=0 by 2
+//rect 1x1
+//rotate row y=0 by 4
+//rect 2x1
+//rotate row y=0 by 2
+//rect 1x1
+//rotate row y=0 by 2
+//rect 1x1
+//rotate row y=0 by 2
+//rect 1x1
+//rotate row y=0 by 3
+//rect 2x1
+//rotate row y=0 by 2
+//rect 1x1
+//rotate row y=0 by 5
+//rect 1x1
+//rotate row y=0 by 2
+//rect 1x1
+//rotate row y=0 by 6
+//rect 5x1
+//rotate row y=0 by 2
+//rect 1x3
+//rotate row y=2 by 8
+//rotate row y=0 by 8
+//rotate column x=0 by 1
+//rect 7x1
+//rotate row y=2 by 24
+//rotate row y=0 by 20
+//rotate column x=5 by 1
+//rotate column x=4 by 2
+//rotate column x=2 by 2
+//rotate column x=0 by 1
+//rect 7x1
+//rotate column x=34 by 2
+//rotate column x=22 by 1
+//rotate column x=15 by 1
+//rotate row y=2 by 18
+//rotate row y=0 by 12
+//rotate column x=8 by 2
+//rotate column x=7 by 1
+//rotate column x=5 by 2
+//rotate column x=2 by 1
+//rotate column x=0 by 1
+//rect 9x1
+//rotate row y=3 by 28
+//rotate row y=1 by 28
+//rotate row y=0 by 20
+//rotate column x=18 by 1
+//rotate column x=15 by 1
+//rotate column x=14 by 1
+//rotate column x=13 by 1
+//rotate column x=12 by 2
+//rotate column x=10 by 3
+//rotate column x=8 by 1
+//rotate column x=7 by 2
+//rotate column x=6 by 1
+//rotate column x=5 by 1
+//rotate column x=3 by 1
+//rotate column x=2 by 2
+//rotate column x=0 by 1
+//rect 19x1
+//rotate column x=34 by 2
+//rotate column x=24 by 1
+//rotate column x=23 by 1
+//rotate column x=14 by 1
+//rotate column x=9 by 2
+//rotate column x=4 by 2
+//rotate row y=3 by 5
+//rotate row y=2 by 3
+//rotate row y=1 by 7
+//rotate row y=0 by 5
+//rotate column x=0 by 2
+//rect 3x2
+//rotate column x=16 by 2
+//rotate row y=3 by 27
+//rotate row y=2 by 5
+//rotate row y=0 by 20
+//rotate column x=8 by 2
+//rotate column x=7 by 1
+//rotate column x=5 by 1
+//rotate column x=3 by 3
+//rotate column x=2 by 1
+//rotate column x=1 by 2
+//rotate column x=0 by 1
+//rect 9x1
+//rotate row y=4 by 42
+//rotate row y=3 by 40
+//rotate row y=1 by 30
+//rotate row y=0 by 40
+//rotate column x=37 by 2
+//rotate column x=36 by 3
+//rotate column x=35 by 1
+//rotate column x=33 by 1
+//rotate column x=32 by 1
+//rotate column x=31 by 3
+//rotate column x=30 by 1
+//rotate column x=28 by 1
+//rotate column x=27 by 1
+//rotate column x=25 by 1
+//rotate column x=23 by 3
+//rotate column x=22 by 1
+//rotate column x=21 by 1
+//rotate column x=20 by 1
+//rotate column x=18 by 1
+//rotate column x=17 by 1
+//rotate column x=16 by 3
+//rotate column x=15 by 1
+//rotate column x=13 by 1
+//rotate column x=12 by 1
+//rotate column x=11 by 2
+//rotate column x=10 by 1
+//rotate column x=8 by 1
+//rotate column x=7 by 2
+//rotate column x=5 by 1
+//rotate column x=3 by 3
+//rotate column x=2 by 1
+//rotate column x=1 by 1
+//rotate column x=0 by 1
+//rect 39x1
+//rotate column x=44 by 2
+//rotate column x=42 by 2
+//rotate column x=35 by 5
+//rotate column x=34 by 2
+//rotate column x=32 by 2
+//rotate column x=29 by 2
+//rotate column x=25 by 5
+//rotate column x=24 by 2
+//rotate column x=19 by 2
+//rotate column x=15 by 4
+//rotate column x=14 by 2
+//rotate column x=12 by 3
+//rotate column x=9 by 2
+//rotate column x=5 by 5
+//rotate column x=4 by 2
+//rotate row y=5 by 5
+//rotate row y=4 by 38
+//rotate row y=3 by 10
+//rotate row y=2 by 46
+//rotate row y=1 by 10
+//rotate column x=48 by 4
+//rotate column x=47 by 3
+//rotate column x=46 by 3
+//rotate column x=45 by 1
+//rotate column x=43 by 1
+//rotate column x=37 by 5
+//rotate column x=36 by 5
+//rotate column x=35 by 4
+//rotate column x=33 by 1
+//rotate column x=32 by 5
+//rotate column x=31 by 5
+//rotate column x=28 by 5
+//rotate column x=27 by 5
+//rotate column x=26 by 3
+//rotate column x=25 by 4
+//rotate column x=23 by 1
+//rotate column x=17 by 5
+//rotate column x=16 by 5
+//rotate column x=13 by 1
+//rotate column x=12 by 5
+//rotate column x=11 by 5
+//rotate column x=3 by 1
+//rotate column x=0 by 1";
 
-var smallInput =
-@"rect 3x2
-rotate column x=1 by 1
-rotate row y=0 by 4
-rotate column x=1 by 1";
+//var smallInput =
+//@"rect 3x2
+//rotate column x=1 by 1
+//rotate row y=0 by 4
+//rotate column x=1 by 1";
 
-var smallest =
-@"";
+//var smallest =
+//@"";
 
-var input = smallInput;
-input = fullInput;
-//input = smallest;
-var timer = System.Diagnostics.Stopwatch.StartNew();
+//var input = smallInput;
+//input = fullInput;
+////input = smallest;
+//var timer = System.Diagnostics.Stopwatch.StartNew();
 
-var width = 50;
-var height = 6;
-var grid = Enumerable.Range(0, width).Select(x => Enumerable.Range(0, height).Select(y => new Cell { X = x, Y = y, Value = false })).SelectMany(x => x).ToList();
-var result = 0;
+//var width = 50;
+//var height = 6;
+//var grid = Enumerable.Range(0, width).Select(x => Enumerable.Range(0, height).Select(y => new Cell { X = x, Y = y, Value = false })).SelectMany(x => x).ToList();
+//var result = 0;
 
-foreach (var line in input.Split(Environment.NewLine))
-{
-    if (line.StartsWith("rect"))
-    {
-        var split = line.Replace("rect ", "").Split("x").Select(int.Parse);
-        var x = split.First();
-        var y = split.Last();
-        for (var i = 0; i < x; i++)
-        {
-            for (var j = 0; j < y; j++)
-            {
-                var cell = grid.Single(x => x.X == i && x.Y == j);
-                cell.Value = true;
-            }
-        }
-    }
+//foreach (var line in input.Split(Environment.NewLine))
+//{
+//    if (line.StartsWith("rect"))
+//    {
+//        var split = line.Replace("rect ", "").Split("x").Select(int.Parse);
+//        var x = split.First();
+//        var y = split.Last();
+//        for (var i = 0; i < x; i++)
+//        {
+//            for (var j = 0; j < y; j++)
+//            {
+//                var cell = grid.Single(x => x.X == i && x.Y == j);
+//                cell.Value = true;
+//            }
+//        }
+//    }
 
-    if (line.StartsWith("rotate column x="))
-    {
-        var split = line.Replace("rotate column x=", "").Split(" by ").Select(int.Parse);
-        var columnIndex = split.First();
-        var by = split.Last();
+//    if (line.StartsWith("rotate column x="))
+//    {
+//        var split = line.Replace("rotate column x=", "").Split(" by ").Select(int.Parse);
+//        var columnIndex = split.First();
+//        var by = split.Last();
 
-        var column = grid.Where(x => x.X == columnIndex);
-        var positions = column.Where(x => x.Value).Select(x => (x.Y + by) % height).ToList();
-        foreach (var item in column)
-        {
-            item.Value = positions.Contains(item.Y);
-        }
-    }
+//        var column = grid.Where(x => x.X == columnIndex);
+//        var positions = column.Where(x => x.Value).Select(x => (x.Y + by) % height).ToList();
+//        foreach (var item in column)
+//        {
+//            item.Value = positions.Contains(item.Y);
+//        }
+//    }
 
-    if (line.StartsWith("rotate row y="))
-    {
-        var split = line.Replace("rotate row y=", "").Split(" by ").Select(int.Parse);
-        var rowIndex = split.First();
-        var by = split.Last();
+//    if (line.StartsWith("rotate row y="))
+//    {
+//        var split = line.Replace("rotate row y=", "").Split(" by ").Select(int.Parse);
+//        var rowIndex = split.First();
+//        var by = split.Last();
 
-        var row = grid.Where(x => x.Y == rowIndex);
-        var positions = row.Where(x => x.Value).Select(x => (x.X + by) % width).ToList();
-        foreach (var item in row)
-        {
-            item.Value = positions.Contains(item.X);
-        }
-    }
+//        var row = grid.Where(x => x.Y == rowIndex);
+//        var positions = row.Where(x => x.Value).Select(x => (x.X + by) % width).ToList();
+//        foreach (var item in row)
+//        {
+//            item.Value = positions.Contains(item.X);
+//        }
+//    }
 
-    Utils.PrintGrid(grid, x => x.X, x => x.Y, x => x.Value ? "#" : ".");
-}
+//    Utils.PrintGrid(grid, x => x.X, x => x.Y, x => x.Value ? "#" : ".");
+//}
 
-result = grid.Count(x => x.Value);
+//result = grid.Count(x => x.Value);
 
-timer.Stop();
-Console.WriteLine(result);
-Console.WriteLine(timer.ElapsedMilliseconds + "ms");
-Console.ReadLine();
+//timer.Stop();
+//Console.WriteLine(result);
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+//Console.ReadLine();
 
-class Cell
-{
-    public int X { get; set; }
-    public int Y { get; set; }
-    public bool Value { get; set; }
-} 
+//class Cell
+//{
+//    public int X { get; set; }
+//    public int Y { get; set; }
+//    public bool Value { get; set; }
+//} 
