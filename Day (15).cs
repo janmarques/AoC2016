65a360ee60e106952d1041b72d242191237b33fd
Jan Marques <jan@janmarques.net>
2024-12-27 22:56:05 +0100

    d16p2

--- Day (15).cs
+++ Day (15).cs
@@ -1,53 +1,53 @@
-﻿var fullInput =
-@"Disc #1 has 17 positions; at time=0, it is at position 15.
-Disc #2 has 3 positions; at time=0, it is at position 2.
-Disc #3 has 19 positions; at time=0, it is at position 4.
-Disc #4 has 13 positions; at time=0, it is at position 2.
-Disc #5 has 7 positions; at time=0, it is at position 2.
-Disc #6 has 5 positions; at time=0, it is at position 0.
-Disc #7 has 11 positions; at time=0, it is at position 0.";
+﻿//var fullInput =
+//@"Disc #1 has 17 positions; at time=0, it is at position 15.
+//Disc #2 has 3 positions; at time=0, it is at position 2.
+//Disc #3 has 19 positions; at time=0, it is at position 4.
+//Disc #4 has 13 positions; at time=0, it is at position 2.
+//Disc #5 has 7 positions; at time=0, it is at position 2.
+//Disc #6 has 5 positions; at time=0, it is at position 0.
+//Disc #7 has 11 positions; at time=0, it is at position 0.";
 
-var smallInput =
-@"Disc #1 has 5 positions; at time=0, it is at position 4.
-Disc #2 has 2 positions; at time=0, it is at position 1.";
+//var smallInput =
+//@"Disc #1 has 5 positions; at time=0, it is at position 4.
+//Disc #2 has 2 positions; at time=0, it is at position 1.";
 
-var smallest =
-@"";
+//var smallest =
+//@"";
 
-var input = smallInput;
-input = fullInput;
-//input = smallest;
-var timer = System.Diagnostics.Stopwatch.StartNew();
+//var input = smallInput;
+//input = fullInput;
+////input = smallest;
+//var timer = System.Diagnostics.Stopwatch.StartNew();
 
-var result = 0;
+//var result = 0;
 
-var discs = input.Split(Environment.NewLine)
-    .Select(x => x.Split(new[] { "Disc #", " has ", " positions; at time=0, it is at position ", "." }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray())
-    .Select(x => new Disc { Rank = x[0], PositionCount = x[1], Position = x[2] })
-    .ToList();
+//var discs = input.Split(Environment.NewLine)
+//    .Select(x => x.Split(new[] { "Disc #", " has ", " positions; at time=0, it is at position ", "." }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray())
+//    .Select(x => new Disc { Rank = x[0], PositionCount = x[1], Position = x[2] })
+//    .ToList();
 
-while (true)
-{
-    if (discs.All(Passes))
-    {
-        break;
-    }
-    result++;
-}
+//while (true)
+//{
+//    if (discs.All(Passes))
+//    {
+//        break;
+//    }
+//    result++;
+//}
 
-bool Passes(Disc disc)
-{
-    return (result + disc.Position + disc.Rank) % disc.PositionCount == 0;
-}
+//bool Passes(Disc disc)
+//{
+//    return (result + disc.Position + disc.Rank) % disc.PositionCount == 0;
+//}
 
-timer.Stop();
-Console.WriteLine(result);
-Console.WriteLine(timer.ElapsedMilliseconds + "ms");
-Console.ReadLine();
+//timer.Stop();
+//Console.WriteLine(result);
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+//Console.ReadLine();
 
-class Disc
-{
-    public int Rank { get; set; }
-    public int PositionCount { get; set; }
-    public int Position { get; set; }
-}
+//class Disc
+//{
+//    public int Rank { get; set; }
+//    public int PositionCount { get; set; }
+//    public int Position { get; set; }
+//}
--- Day (16).cs
+++ Day (16).cs
@@ -1,21 +1,49 @@
-﻿//var fullInput =
-//@"";
+﻿using System.Text;
 
-//var smallInput =
-//@"";
+var fullInput =
+@"10111100110001111";
 
-//var smallest = "";
+var smallInput =
+@"10000";
 
-//var input = smallInput;
-////var input = fullInput;
-////var input = smallest;
+var smallest =
+@"";
 
-//var result = "";
+var input = smallInput;
+input = fullInput;
+//input = smallest;
+var timer = System.Diagnostics.Stopwatch.StartNew();
 
-//foreach (var line in input.Split(Environment.NewLine))
-//{
+var result = "";
+var length = 35651584;
 
-//}
+var a = input.Select(x => x == '1').ToArray();
 
-//Console.WriteLine(result);
-//Console.ReadLine();
+while (a.Length < length)
+{
+    a = a.Concat(new[] { false }).Concat(a.Reverse().Select(x => !x)).ToArray();
+}
+
+//Console.WriteLine(string.Join("", a.Select(x => x ? "1" : "0")));
+
+var checksum = a.Take(length).ToArray();
+
+int k = 0;
+while (checksum.Length % 2 == 0)
+{
+    //Console.WriteLine(k.ToString());
+    k++;
+    var newChecksum = new bool[checksum.Length / 2];
+    for (int i = 0; i < checksum.Length - 1; i = i + 2)
+    {
+        newChecksum[i/2] = checksum[i] == checksum[i + 1];
+    }
+    checksum = newChecksum;
+}
+
+result = string.Join("", checksum.Select(x => x ? "1" : "0"));
+
+timer.Stop();
+Console.WriteLine(result);
+Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+Console.ReadLine();
