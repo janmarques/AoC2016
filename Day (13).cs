f5c86ac18e0ef0661213089fca881452b1cb38e5
Jan Marques <jan@janmarques.net>
2024-12-27 21:26:03 +0100

    d14p1

--- Day (13).cs
+++ Day (13).cs
@@ -1,83 +1,83 @@
-﻿using AoC2023;
-using AoC2024;
-using System.Collections;
-using System.Numerics;
+﻿//using AoC2023;
+//using AoC2024;
+//using System.Collections;
+//using System.Numerics;
 
-var fullInput = 1358;
+//var fullInput = 1358;
 
-var smallInput = 10;
+//var smallInput = 10;
 
-var smallest =
-@"";
+//var smallest =
+//@"";
 
-var input = smallInput;
-input = fullInput;
-//input = smallest;
-var timer = System.Diagnostics.Stopwatch.StartNew();
+//var input = smallInput;
+//input = fullInput;
+////input = smallest;
+//var timer = System.Diagnostics.Stopwatch.StartNew();
 
-var result = int.MaxValue;
+//var result = int.MaxValue;
 
-var target = (x: 7, y: 4);
-if (input > 10)
-{
-    target = (x: 31, y: 39);
-}
+//var target = (x: 7, y: 4);
+//if (input > 10)
+//{
+//    target = (x: 31, y: 39);
+//}
 
-var position = (x: 1, y: 1);
+//var position = (x: 1, y: 1);
 
-var pq = new PrioritySet<State, long>();
-pq.Enqueue(new State { Position = position, Steps = 0, Visited = new HashSet<(int x, int y)>() }, 0);
-var visitable = new HashSet<(int, int)> { position };
+//var pq = new PrioritySet<State, long>();
+//pq.Enqueue(new State { Position = position, Steps = 0, Visited = new HashSet<(int x, int y)>() }, 0);
+//var visitable = new HashSet<(int, int)> { position };
 
-while (pq.Count > 0)
-{
-    var state = pq.Dequeue();
-    if (state.Steps > 50)
-    {
-        break;
-    }
-    if (state.Steps <= 50)
-    {
-        visitable.Add(state.Position);
-    }
+//while (pq.Count > 0)
+//{
+//    var state = pq.Dequeue();
+//    if (state.Steps > 50)
+//    {
+//        break;
+//    }
+//    if (state.Steps <= 50)
+//    {
+//        visitable.Add(state.Position);
+//    }
 
-    foreach (var item in Utils.Directions)
-    {
-        var next = (x: state.Position.x + item.x, y: state.Position.y + item.y);
-        if (next.x < 0 || next.y < 0) { continue; }
-        if (IsWall(next.x, next.y)) { continue; }
-        if (state.Visited.Contains(next)) { continue; }
+//    foreach (var item in Utils.Directions)
+//    {
+//        var next = (x: state.Position.x + item.x, y: state.Position.y + item.y);
+//        if (next.x < 0 || next.y < 0) { continue; }
+//        if (IsWall(next.x, next.y)) { continue; }
+//        if (state.Visited.Contains(next)) { continue; }
 
-        var newState = state.Clone();
-        newState.Visited.Add(next);
-        newState.Position = next;
-        newState.Steps++;
-        pq.Enqueue(newState, newState.Steps);
-    }
-}
+//        var newState = state.Clone();
+//        newState.Visited.Add(next);
+//        newState.Position = next;
+//        newState.Steps++;
+//        pq.Enqueue(newState, newState.Steps);
+//    }
+//}
 
-result = visitable.Count;
+//result = visitable.Count;
 
-bool IsWall(int x, int y)
-{
-    var p = x * x + 3 * x + 2 * x * y + y + y * y;
-    p += input;
-    var ba = new BitArray(new[] { p });
-    var a = new bool[ba.Count];
-    ba.CopyTo(a, 0);
-    return a.Count(x => x) % 2 == 1;
-}
+//bool IsWall(int x, int y)
+//{
+//    var p = x * x + 3 * x + 2 * x * y + y + y * y;
+//    p += input;
+//    var ba = new BitArray(new[] { p });
+//    var a = new bool[ba.Count];
+//    ba.CopyTo(a, 0);
+//    return a.Count(x => x) % 2 == 1;
+//}
 
-timer.Stop();
-Console.WriteLine(result);
-Console.WriteLine(timer.ElapsedMilliseconds + "ms");
-Console.ReadLine();
+//timer.Stop();
+//Console.WriteLine(result);
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+//Console.ReadLine();
 
-class State
-{
-    public (int x, int y) Position { get; set; }
-    public HashSet<(int x, int y)> Visited { get; set; }
-    public int Steps { get; set; }
+//class State
+//{
+//    public (int x, int y) Position { get; set; }
+//    public HashSet<(int x, int y)> Visited { get; set; }
+//    public int Steps { get; set; }
 
-    public State Clone() => new State { Position = Position, Steps = Steps, Visited = Visited.ToHashSet() };
-}
+//    public State Clone() => new State { Position = Position, Steps = Steps, Visited = Visited.ToHashSet() };
+//}
--- Day (14).cs
+++ Day (14).cs
@@ -1,21 +1,75 @@
-﻿//var fullInput =
-//@"";
+﻿var fullInput =
+@"yjdafjpo";
 
-//var smallInput =
-//@"";
+var smallInput =
+@"abc";
 
-//var smallest = "";
+var smallest =
+@"";
 
-//var input = smallInput;
-////var input = fullInput;
-////var input = smallest;
+var input = smallInput;
+input = fullInput;
+//input = smallest;
+var timer = System.Diagnostics.Stopwatch.StartNew();
 
-//var result = "";
+var result = 0;
 
-//foreach (var line in input.Split(Environment.NewLine))
-//{
+var triplets = new Dictionary<int, char>();
+var left = 64;
+for (int i = 0; ; i++)
+{
+    var hash = CreateMD5(input + i);
+    var foundTriplet = false;
+    for (int j = 0; j < hash.Length - 2; j++)
+    {
+        if (hash[j] == hash[j + 1] && hash[j] == hash[j + 2] && !foundTriplet)
+        {
+            triplets[i] = hash[j];
+            foundTriplet = true;
+            Console.WriteLine($"Candidate 3x{hash[j]} #{i} in {hash}");
+        }
+        if (j < hash.Length - 4 && hash[j] == hash[j + 1] && hash[j] == hash[j + 2] && hash[j] == hash[j + 3] && hash[j] == hash[j + 4])
+        {
+            var corresponding = triplets.Where(x => x.Key > i - 1000 && x.Key != i && x.Value == hash[j]).ToList();
+            foreach (var triplet in corresponding)
+            {
+                left--;
+                triplets.Remove(triplet.Key);
 
-//}
+                Console.WriteLine($"Removing 5x{hash[j]}(#{triplet.Key}) #{i} in {hash}");
 
-//Console.WriteLine(result);
-//Console.ReadLine();
+                if (left == 0)
+                {
+                    result = triplet.Key;
+                    goto end;
+                }
+            }
+        }
+    }
+}
+end:;
+timer.Stop();
+Console.WriteLine(result);
+Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+Console.ReadLine();
+
+//https://stackoverflow.com/questions/11454004/calculate-a-md5-hash-from-a-string
+string CreateMD5(string input)
+{
+    // Use input string to calculate MD5 hash
+    using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+    {
+        byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+        byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+        return Convert.ToHexString(hashBytes); // .NET 5 +
+
+        // Convert the byte array to hexadecimal string prior to .NET 5
+        // StringBuilder sb = new System.Text.StringBuilder();
+        // for (int i = 0; i < hashBytes.Length; i++)
+        // {
+        //     sb.Append(hashBytes[i].ToString("X2"));
+        // }
+        // return sb.ToString();
+    }
+}
