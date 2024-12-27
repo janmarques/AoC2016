1fcd76b5922f44542168e8c18392aad1a88f02c7
Jan Marques <jan@janmarques.net>
2024-12-27 19:08:59 +0100

    d11p1

--- PrioritySet.cs
+++ PrioritySet.cs
@@ -18,3 +18,23 @@ public class PrioritySet<T, K>
         return false;
     }
 }
+
+public class PrioritySetHashed<T, K>(Func<T, string> hashSelector)
+{
+    public PriorityQueue<T, K> Queue { get; set; } = new PriorityQueue<T, K>();
+    public HashSet<string> Set { get; set; } = new HashSet<string>();
+
+    public int Count => Queue.Count;
+    public T Dequeue() => Queue.Dequeue();
+    public bool Enqueue(T item, K prio)
+    {
+        var hash = hashSelector(item);
+        if (!Set.Contains(hash))
+        {
+            Queue.Enqueue(item, prio);
+            Set.Add(hash);
+            return true;
+        }
+        return false;
+    }
+}
