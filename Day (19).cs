cf2318d4507ea2dfef348e0837642de9ef606654
Jan Marques <jan@janmarques.net>
2024-12-28 14:50:20 +0100

    d

--- Day (19).cs
+++ Day (19).cs
@@ -1,21 +1,32 @@
-﻿//var fullInput =
-//@"";
+﻿var fullInput =
+3014603;
 
-//var smallInput =
-//@"";
+var smallInput =
+3;
 
-//var smallest = "";
+var smallest =
+@"";
 
-//var input = smallInput;
-////var input = fullInput;
-////var input = smallest;
+var input = smallInput;
+input = fullInput;
+//input = smallest;
+var timer = System.Diagnostics.Stopwatch.StartNew();
 
-//var result = "";
+var result = 0;
 
-//foreach (var line in input.Split(Environment.NewLine))
-//{
+var items = Enumerable.Range(1, input).ToList();
+var i = 1;
+while (true)
+{
+    if(i == items.Count) { i = 0; }
+    if (items.Count == 1) { break; }
+    items.RemoveAt(i);
+    i++;
+}
 
-//}
+result = items.Single();
 
-//Console.WriteLine(result);
-//Console.ReadLine();
+timer.Stop();
+Console.WriteLine(result);
+Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+Console.ReadLine();
