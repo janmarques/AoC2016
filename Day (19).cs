69b81bbe8eecc642d99e3c941815735b87536152
Jan Marques <jan@janmarques.net>
2024-12-28 20:13:53 +0100

    d20p1

--- Day (19).cs
+++ Day (19).cs
@@ -1,45 +1,45 @@
-﻿using AoC2024;
+﻿//using AoC2024;
 
-var fullInput =
-3014603;
+//var fullInput =
+//3014603;
 
-var smallInput =
-7;
+//var smallInput =
+//7;
 
-var smallest =
-@"";
+//var smallest =
+//@"";
 
-var input = smallInput;
-input = fullInput;
-//input = smallest;
-var timer = System.Diagnostics.Stopwatch.StartNew();
+//var input = smallInput;
+//input = fullInput;
+////input = smallest;
+//var timer = System.Diagnostics.Stopwatch.StartNew();
 
-var result = 0;
+//var result = 0;
 
-var items = Enumerable.Range(1, input).ToList();
+//var items = Enumerable.Range(1, input).ToList();
 
-int k = 0;
-while (items.Count() > 1)
-{
-    Utils.Counter("items", 1000, extraText: () => items.Count().ToString());
+//int k = 0;
+//while (items.Count() > 1)
+//{
+//    Utils.Counter("items", 1000, extraText: () => items.Count().ToString());
 
-    var remove = (items.Count / 2 + k) % items.Count;
-    //Console.WriteLine($"{items[k]} removing {items[remove]}");
-    items.RemoveAt(remove);
-    if (remove < k)
-    {
-        k %= items.Count;
-    }
-    else
-    {
-        k++;
-        k %= items.Count;
-    }
-}
+//    var remove = (items.Count / 2 + k) % items.Count;
+//    //Console.WriteLine($"{items[k]} removing {items[remove]}");
+//    items.RemoveAt(remove);
+//    if (remove < k)
+//    {
+//        k %= items.Count;
+//    }
+//    else
+//    {
+//        k++;
+//        k %= items.Count;
+//    }
+//}
 
-result = items.Single();
+//result = items.Single();
 
-timer.Stop();
-Console.WriteLine(result);
-Console.WriteLine(timer.ElapsedMilliseconds + "ms");
-Console.ReadLine();
+//timer.Stop();
+//Console.WriteLine(result);
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+//Console.ReadLine();
