0156325615a6e45c40e72265c19a2e7bb2d91290
Jan Marques <jan@janmarques.net>
2024-12-28 14:24:31 +0100

    d18p1

--- Day (17).cs
+++ Day (17).cs
@@ -1,72 +1,72 @@
-﻿using AoC2023;
-using AoC2024;
-using System.Collections;
-using System.Numerics;
+﻿//using AoC2023;
+//using AoC2024;
+//using System.Collections;
+//using System.Numerics;
 
-var fullInput = "dmypynyp";
+//var fullInput = "dmypynyp";
 
-var smallInput = "ihgpwlah";
+//var smallInput = "ihgpwlah";
 
-var smallest =
-@"";
+//var smallest =
+//@"";
 
-var input = smallInput;
-input = fullInput;
-var timer = System.Diagnostics.Stopwatch.StartNew();
+//var input = smallInput;
+//input = fullInput;
+//var timer = System.Diagnostics.Stopwatch.StartNew();
 
-var result = int.MinValue;
+//var result = int.MinValue;
 
-var pq = new PriorityQueue<(string path, int x, int y), long>();
-pq.Enqueue(("", 0, 0), 0);
+//var pq = new PriorityQueue<(string path, int x, int y), long>();
+//pq.Enqueue(("", 0, 0), 0);
 
-while (pq.Count > 0)
-{
-    (string path, int x, int y) = pq.Dequeue();
+//while (pq.Count > 0)
+//{
+//    (string path, int x, int y) = pq.Dequeue();
 
-    var steps = path.Length;
-    Utils.Counter("pq", 50_000, extraText: $"{steps} {result} {pq.Count}");
-    if (steps > 1500)
-    {
-        break;
-    }
-    if (x == 3 && y == 3)
-    {
-        result = Math.Max(result, steps);
-        continue;
-    }
+//    var steps = path.Length;
+//    Utils.Counter("pq", 50_000, extraText: $"{steps} {result} {pq.Count}");
+//    if (steps > 1500)
+//    {
+//        break;
+//    }
+//    if (x == 3 && y == 3)
+//    {
+//        result = Math.Max(result, steps);
+//        continue;
+//    }
 
-    void TryEnqueue(char dir, char item, int xOffset, int yOffset)
-    {
-        if (item == 'B' || item == 'C' || item == 'D' || item == 'E' || item == 'F')
-        {
-            var newX = x + xOffset;
-            var newY = y + yOffset;
-            if (newX < 0 || newY < 0) { return; }
-            if (newX > 3 || newY > 3) { return; }
-            pq.Enqueue((path + dir, newX, newY), steps + 1);
-        }
-    }
+//    void TryEnqueue(char dir, char item, int xOffset, int yOffset)
+//    {
+//        if (item == 'B' || item == 'C' || item == 'D' || item == 'E' || item == 'F')
+//        {
+//            var newX = x + xOffset;
+//            var newY = y + yOffset;
+//            if (newX < 0 || newY < 0) { return; }
+//            if (newX > 3 || newY > 3) { return; }
+//            pq.Enqueue((path + dir, newX, newY), steps + 1);
+//        }
+//    }
 
-    var md5 = CreateMD5(input + path);
-    TryEnqueue('U', md5[0], 0, -1);
-    TryEnqueue('D', md5[1], 0, 1);
-    TryEnqueue('L', md5[2], -1, 0);
-    TryEnqueue('R', md5[3], 1, 0);
-}
-
-
-timer.Stop();
-Console.WriteLine(result);
-Console.WriteLine(timer.ElapsedMilliseconds + "ms");
-Console.ReadLine();
+//    var md5 = CreateMD5(input + path);
+//    TryEnqueue('U', md5[0], 0, -1);
+//    TryEnqueue('D', md5[1], 0, 1);
+//    TryEnqueue('L', md5[2], -1, 0);
+//    TryEnqueue('R', md5[3], 1, 0);
+//}
 
 
+//timer.Stop();
+//Console.WriteLine(result);
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+//Console.ReadLine();
 
-//https://stackoverflow.com/questions/11454004/calculate-a-md5-hash-from-a-string
-string CreateMD5(string input)
-{
-    var z = System.Text.Encoding.ASCII.GetBytes(input);
-    var a = System.Security.Cryptography.MD5.HashData(z)[0..4];
-    var b = Convert.ToHexString(a);
-    return b;
-}
+
+
+////https://stackoverflow.com/questions/11454004/calculate-a-md5-hash-from-a-string
+//string CreateMD5(string input)
+//{
+//    var z = System.Text.Encoding.ASCII.GetBytes(input);
+//    var a = System.Security.Cryptography.MD5.HashData(z)[0..4];
+//    var b = Convert.ToHexString(a);
+//    return b;
+//}
--- Day (18).cs
+++ Day (18).cs
@@ -1,21 +1,63 @@
-﻿//var fullInput =
-//@"";
+﻿var fullInput =
+@".^..^....^....^^.^^.^.^^.^.....^.^..^...^^^^^^.^^^^.^.^^^^^^^.^^^^^..^.^^^.^^..^.^^.^....^.^...^^.^.";
 
-//var smallInput =
-//@"";
+var smallInput =
+@".^^.^.^^^^";
 
-//var smallest = "";
+var smallest =
+@"";
 
-//var input = smallInput;
-////var input = fullInput;
-////var input = smallest;
+var input = smallInput;
+input = fullInput;
+//input = smallest;
+var timer = System.Diagnostics.Stopwatch.StartNew();
 
-//var result = "";
+var result = 0;
 
-//foreach (var line in input.Split(Environment.NewLine))
-//{
+var line = input.Select(x => x == '^').ToArray();
 
-//}
+var rowCount = 40;
 
-//Console.WriteLine(result);
-//Console.ReadLine();
+var lines = new List<bool[]>();
+lines.Add(line);
+Console.WriteLine(string.Join("", line.Select(x => x ? "^" : ".")));
+
+for (int i = 0; i < rowCount - 1; i++)
+{
+    var newLine = new bool[line.Length];
+    for (int j = 0; j < line.Length; j++)
+    {
+        var left = false;
+        var center = false;
+        var right = false;
+        if (j == 0)
+        {
+            center = line[j];
+            right = line[j + 1];
+        }
+        else if (j == line.Length - 1)
+        {
+            left = line[j - 1];
+            center = line[j];
+        }
+        else
+        {
+            left = line[j - 1];
+            center = line[j];
+            right = line[j + 1];
+        }
+
+        newLine[j] = (left && center && !right) || (center && right && !left) || (left && !right && !center) || (right && !left && !center);
+    }
+    line = newLine;
+
+    Console.WriteLine(string.Join("", line.Select(x => x ? "^" : ".")));
+    lines.Add(line);
+}
+
+result = lines.Sum(x => x.Count(y => !y));
+
+timer.Stop();
+Console.WriteLine(result);
+Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+Console.ReadLine();
