f9770582c55c798e3138fb04e4a81bc802307df0
Jan Marques <jan@janmarques.net>
2024-12-28 11:17:07 +0100

    d17p1 example not ok, real yes

--- Day (16).cs
+++ Day (16).cs
@@ -1,53 +1,53 @@
-﻿using System.Text;
+﻿//using System.Text;
 
-var fullInput =
-@"10111100110001111";
+//var fullInput =
+//@"10111100110001111";
 
-var smallInput =
-@"10000";
+//var smallInput =
+//@"10000";
 
-var smallest =
-@"";
+//var smallest =
+//@"";
 
-var input = smallInput;
-input = fullInput;
-//input = smallest;
-var timer = System.Diagnostics.Stopwatch.StartNew();
+//var input = smallInput;
+//input = fullInput;
+////input = smallest;
+//var timer = System.Diagnostics.Stopwatch.StartNew();
 
-var result = "";
-var length = 35651584;
+//var result = "";
+//var length = 35651584;
 
-var cnt = input.Length;
+//var cnt = input.Length;
 
-var arr = new bool[length];
-Array.Copy(input.Select(x => x == '1').ToArray(), arr, cnt);
+//var arr = new bool[length];
+//Array.Copy(input.Select(x => x == '1').ToArray(), arr, cnt);
 
-while (cnt < length)
-{
-    arr[cnt] = false;
-    Array.Copy(arr[0..(cnt)].Reverse().Select(x => !x).ToArray(), 0, arr, cnt + 1, Math.Min(cnt, arr.Length - cnt-1));
-    cnt *= 2;
-    cnt++;
-}
+//while (cnt < length)
+//{
+//    arr[cnt] = false;
+//    Array.Copy(arr[0..(cnt)].Reverse().Select(x => !x).ToArray(), 0, arr, cnt + 1, Math.Min(cnt, arr.Length - cnt-1));
+//    cnt *= 2;
+//    cnt++;
+//}
 
-//Console.WriteLine(string.Join("", arr.Select(x => x ? "1" : "0")));
+////Console.WriteLine(string.Join("", arr.Select(x => x ? "1" : "0")));
 
-var checksum = arr.Take(length).ToArray();
-int k = 0;
-while (length % 2 == 0)
-{
-    //Console.WriteLine(k.ToString());
-    k++;
-    for (int i = 0; i < length - 1; i = i + 2)
-    {
-        checksum[i / 2] = checksum[i] == checksum[i + 1];
-    }
-    length /= 2;
-}
+//var checksum = arr.Take(length).ToArray();
+//int k = 0;
+//while (length % 2 == 0)
+//{
+//    //Console.WriteLine(k.ToString());
+//    k++;
+//    for (int i = 0; i < length - 1; i = i + 2)
+//    {
+//        checksum[i / 2] = checksum[i] == checksum[i + 1];
+//    }
+//    length /= 2;
+//}
 
-result = string.Join("", checksum.Take(length).Select(x => x ? "1" : "0"));
+//result = string.Join("", checksum.Take(length).Select(x => x ? "1" : "0"));
 
-timer.Stop();
-Console.WriteLine(result);
-Console.WriteLine(timer.ElapsedMilliseconds + "ms");
-Console.ReadLine();
+//timer.Stop();
+//Console.WriteLine(result);
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+//Console.ReadLine();
--- Day (17).cs
+++ Day (17).cs
@@ -1,21 +1,85 @@
-﻿//var fullInput =
-//@"";
+﻿using AoC2023;
+using AoC2024;
+using System.Collections;
+using System.Numerics;
 
-//var smallInput =
-//@"";
+var fullInput = "dmypynyp";
 
-//var smallest = "";
+var smallInput = "ulqzkmiv";
 
-//var input = smallInput;
-////var input = fullInput;
-////var input = smallest;
+var smallest =
+@"";
 
-//var result = "";
+var input = smallInput;
+input = fullInput;
+var timer = System.Diagnostics.Stopwatch.StartNew();
 
-//foreach (var line in input.Split(Environment.NewLine))
-//{
+var result = int.MaxValue;
 
-//}
+var target = (x: 3, y: 3);
 
-//Console.WriteLine(result);
-//Console.ReadLine();
+var pq = new PrioritySet<State, long>();
+pq.Enqueue(new State { Path = "" }, 0);
+var results = new HashSet<string> { };
+
+while (pq.Count > 0)
+{
+    var state = pq.Dequeue();
+
+    if(state.Path == "DRURDRUDDRDL")
+    {
+
+    }
+    if (results.Any() && state.Steps > results.Min(x => x.Length))
+    {
+        break;
+    }
+    if (state.Position == target)
+    {
+        results.Add(state.Path);
+        continue;
+    }
+
+    void TryEnqueue(char dir, char item)
+    {
+        var isOpen = new[] { 'b', 'c', 'd', 'e', 'f' }.Contains(item);
+        if (isOpen)
+        {
+            var newState = state.Clone();
+            newState.Path += dir;
+            pq.Enqueue(newState, newState.Steps);
+        }
+    }
+
+    var md5 = CreateMD5(input + state.Path);
+    TryEnqueue('U', md5[0]);
+    TryEnqueue('D', md5[1]);
+    TryEnqueue('L', md5[2]);
+    TryEnqueue('R', md5[3]);
+}
+
+Console.WriteLine(results.OrderBy(x => x.Length).First());
+
+
+timer.Stop();
+Console.WriteLine(result);
+Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+Console.ReadLine();
+
+
+
+//https://stackoverflow.com/questions/11454004/calculate-a-md5-hash-from-a-string
+string CreateMD5(string input)
+{
+    using System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+    return Convert.ToHexString(md5.ComputeHash(System.Text.Encoding.ASCII.GetBytes(input))).ToLower();
+}
+
+class State
+{
+    public (int x, int y) Position => (Path.Count(x => x == 'R') - Path.Count(x => x == 'L'), Path.Count(x => x == 'D') - Path.Count(x => x == 'U'));
+    public string Path { get; set; }
+    public int Steps => Path.Length;
+
+    public State Clone() => new State { Path = Path };
+}
