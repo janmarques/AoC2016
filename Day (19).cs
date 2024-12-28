0d80ef18b485f87a6df872812068aa5283499a50
Jan Marques <jan@janmarques.net>
2024-12-28 16:30:30 +0100

    nw

--- Day (19).cs
+++ Day (19).cs
@@ -8,25 +8,34 @@ var smallest =
 @"";
 
 var input = smallInput;
-input = fullInput;
+//input = fullInput;
 //input = smallest;
 var timer = System.Diagnostics.Stopwatch.StartNew();
 
 var result = 0;
 
-var items = Enumerable.Range(1, input).ToArray();
-var keepEven = true;
+var items = Enumerable.Range(1, input).ToList();
+
+int k = 0;
 while (items.Count() > 1)
 {
-    var ogLast = items.Last();
-    items = items.Where((x, i) => i % 2 == (keepEven ? 0 : 1)).ToArray();
-
-    keepEven = ogLast != items.Last();
+    var toRemove = new HashSet<int>();
+    for (int i = 0; i < items.Count(); i++)
+    {
+        if (toRemove.Contains(i)) { continue; }
+        var remove = (items.Except(toRemove).Count() / 2 + k) % items.Except(toRemove).Count();
+        toRemove.Add(remove);
+        Console.WriteLine($"{items[i]} removing {items[remove]}");
+    }
+    foreach (var item in toRemove.OrderByDescending(x => x))
+    {
+        items.RemoveAt(item);
+    }
 }
 
 result = items.Single();
 
 timer.Stop();
-Console.WriteLine(result); 
+Console.WriteLine(result); // 188411 low 31820 low
 Console.WriteLine(timer.ElapsedMilliseconds + "ms");
 Console.ReadLine();
